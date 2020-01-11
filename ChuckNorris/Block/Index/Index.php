d0ccf7cd80b3d9805ec31af6c765436b4aa09d07
Gary Constable <garyconstable80@gmail.com>
2020-01-11 14:12:00 +0000

    Added Vue frontend to module.

--- ChuckNorris/Block/Index/Index.php
+++ ChuckNorris/Block/Index/Index.php
@@ -36,9 +36,6 @@ class Index extends \Magento\Framework\View\Element\Template
         \Magento\Framework\View\Element\Template\Context $context,
         array $data = []
     ) {
-
-
-
         parent::__construct($context, $data);
     }
 }
---
+++ ChuckNorris/view/frontend/requirejs-config.js
@@ -0,0 +1,7 @@
+let config = {
+    map: {
+        '*': {
+            Vue: 'PixieMedia_ChuckNorris/js/vue@2.6.11'
+        }
+    }
+};
